14655f6fe2a716c635b54e0dc0e3de768dc35409
BrazenFox5 <80448295+BrazenFox5@users.noreply.github.com>
2021-05-17 20:09:55 -0700

    Delete webpages(18+).bat

--- webpages(18+).bat
+++
@@ -1,3 +0,0 @@
-start https://www.google.com/search?q=sex&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi_9NqIyPLvAhWFrp4KHXEnDT0Q_AUoAnoECAEQBA&biw=950&bih=490
-start https://github.com/BrazenFox5
-https://github.com/BrazenFox5/giv-da-proto-ram-nex-tim
